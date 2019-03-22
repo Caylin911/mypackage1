a609979bf4516a725f2330861ad43d556d359018
Caylin911 <48672588+Caylin911@users.noreply.github.com>
2019-03-22 13:28:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='EDSA example python package',
+    description='mypackage',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
+    url='git@github.com:Caylin911/mypackage.git',
     author='Caylin Gabriel',
     author_email='caylingab@gmail.com'
 )
